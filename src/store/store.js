d751d57e1b714eeb755f3feb4873f25ee1182817
Grattade07 <joe.woodcock15@hotmail.co.uk>
2023-02-25 20:35:59 +0000

    Delete src directory

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit"
-import carDatabaseReducer from "./carDatabase"
-
-export default configureStore({
-    reducer: {
-        carDatabase: carDatabaseReducer,
-    },
-})
